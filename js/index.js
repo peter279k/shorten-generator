626da101bceba3cf159710eb8777d1e8b0abcafc
peter279k <peter279k@gmail.com>
2016-01-31 12:22:28 +0800

    add select-menu and sel-service

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 $(function() {
 	$("#post-url").click(function() {
 		if ($("#longUrl").val() == "") {
-			$(".container").append('<div class="alert alert-warning">'+
-  			'<strong>Oops!</strong> 尚未輸入網址 !'+
+			$(".container").append('<div class="form-group alert alert-warning">'+
+  				'<strong>Oops!</strong> 尚未輸入網址 !'+
 			"</div>");
+		}
+		else {
+			$.post("/shorten-generator/", {"longUrl": $("#longUrl").val(), ""}, function(response) {
+
+			});
 		}
 	});
 });
