25889decc7a86afdd08e24c3a11fa9c43071b981
peter279k <peter279k@gmail.com>
2016-01-31 12:08:41 +0800

    add longUrl check empty

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(function() {
-	alert("XD");
+	$("#post-url").click(function() {
+		if ($("#longUrl").val() == "") {
+			$(".container").append('<div class="alert alert-warning">'+
+  			'<strong>Oops!</strong> 尚未輸入網址 !'+
+			"</div>");
+		}
+	});
 });
