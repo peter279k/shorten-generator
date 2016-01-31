5a6846b43362f59ee00ce3dd67e3666aa0d29b36
peter279k <peter279k@gmail.com>
2016-01-31 12:27:21 +0800

    remove $.post

--- js/index.js
+++ js/index.js
@@ -5,10 +5,12 @@ $(function() {
   				'<strong>Oops!</strong> 尚未輸入網址 !'+
 			"</div>");
 		}
+		else if($("#sel-service").val() === "no-sel") {
+			$(".container").append('<div class="form-group alert alert-warning">'+
+  				'<strong>Oops!</strong> 尚未選擇縮網服務 !'+"</div>");	
+		}
 		else {
-			$.post("/shorten-generator/", {"longUrl": $("#longUrl").val(), ""}, function(response) {
-
-			});
+			
 		}
 	});
 });
