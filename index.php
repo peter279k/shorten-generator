122ba10cbb2df51a6cc83db3682d8dfec4c40bf2
peter279k <peter279k@gmail.com>
2016-01-31 11:41:03 +0800

    using hoa/router

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
-	require __DIR__ . '/vendor/autoload.php';
+	require 'vendor/autoload.php';
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
 
-	// Create Router instance
-	$router = new \Bramus\Router\Router();
+	$router = new League\Route\RouteCollection;
 
-	// Good
-	$router->get('/hello/(\w+)', function($name) {
-		echo 'Hello ' . htmlentities($name);
+	$router->addRoute('GET', '/', function (Request $request, Response $response) {
+		// do something clever
+		if(file_exists("index.html")) {
+    			echo file_get_contents("index.html");
+		}
+		else {
+			echo "index file not found.";
+		}
+		return $response;
 	});
 
-	$router -> run();
+	$dispatcher = $router->getDispatcher();
+
+	$response = $dispatcher->dispatch('GET', '/');
+
+	$response->send();
