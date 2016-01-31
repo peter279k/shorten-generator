313bc820fd420a84ae79397ad494a7217140d219
peter279k <peter279k@gmail.com>
2016-01-31 11:18:27 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	require 'vendor/autoload.php';
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+
+	$router = new League\Route\RouteCollection;
+
+	$router->addRoute('GET', '/', function (Request $request, Response $response) {
+		// do something clever
+		if(file_exists("index.html")) {
+    			echo file_get_contents("index.html");
+		}
+		else {
+			echo "index file not found.";
+		}
+		return $response;
+	});
+
+	$dispatcher = $router->getDispatcher();
+
+	$response = $dispatcher->dispatch('GET', '/acme/route');
+
+	$response->send();
